73e73ea6e96713ce87c9f7e031cf832fa5e05bbe
Norbert Orzechowicz <norbert.orzechowicz@fsi.pl>
2013-04-26 14:35:52 +0200

    Fixed DoctrineResult behavior when paginator contains different data than array of objects.

--- tests/FSi/Component/DataSource/Tests/Driver/Doctrine/DoctrineResultTest.php
+++ tests/FSi/Component/DataSource/Tests/Driver/Doctrine/DoctrineResultTest.php
@@ -29,4 +29,35 @@ class DoctrineResultTest extends \PHPUnit_Framework_TestCase
 
         $result = new DoctrineResult($registry, $paginator);
     }
+
+    public function testResultWithNotObjectDataInRows()
+    {
+        $registry = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+        $paginator = $this->getMockBuilder('Doctrine\ORM\Tools\Pagination\Paginator')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $paginator->expects($this->any())
+            ->method('getIterator')
+            ->will($this->returnValue(array(
+                '0' => array(
+                    'foo',
+                    'bar'
+                ),
+                '1' => array(
+                    'foo1',
+                    'bar1'
+                )
+            )));
+
+        $result = new DoctrineResult($registry, $paginator);
+        $this->assertSame($result['0'], array(
+            'foo',
+            'bar'
+        ));
+        $this->assertSame($result['1'], array(
+            'foo1',
+            'bar1'
+        ));
+    }
 }
