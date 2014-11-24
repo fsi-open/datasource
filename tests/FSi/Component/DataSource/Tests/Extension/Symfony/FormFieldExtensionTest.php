8d3e1066b6e51b3d83943187f2511eae28ddcf7d
Bartlomiej Gruszka <bartlomiej.gruszka@fsi.pl>
2014-11-24 09:56:56 +0100

    Test case: Build form when options not provided

--- tests/FSi/Component/DataSource/Tests/Extension/Symfony/FormFieldExtensionTest.php
+++ tests/FSi/Component/DataSource/Tests/Extension/Symfony/FormFieldExtensionTest.php
@@ -14,7 +14,7 @@ use ReflectionMethod;
 
 class FormFieldExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBuildBooleanForm()
+    public function testBuildBooleanFormWhenOptionsProvided()
     {
         $formFactory = $this->getMockBuilder('Symfony\Component\Form\FormFactory')
             ->disableOriginalConstructor()
@@ -60,6 +60,56 @@ class FormFieldExtensionTest extends \PHPUnit_Framework_TestCase
                 '0' => 'nie'
             )
         );
+
+        $method->invoke(
+            $formFielExtension,
+            $form,
+            $field,
+            $options
+        );
+    }
+
+    public function testBuildBooleanFormWhenOptionsNotProvided()
+    {
+        $formFactory = $this->getMockBuilder('Symfony\Component\Form\FormFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+git s
+        $formFielExtension = new FormFieldExtension($formFactory);
+
+        $method = new ReflectionMethod(
+            'FSi\Component\DataSource\Extension\Symfony\Form\Field\FormFieldExtension',
+            'buildBooleanForm'
+        );
+        $method->setAccessible(true);
+
+        $field = $this->getMock('FSi\Component\DataSource\Driver\Collection\Extension\Core\Field\Boolean');
+
+        $field->expects($this->once())
+            ->method('getname')
+            ->will($this->returnValue('name'));
+
+        $form = $this->getMockBuilder('Symfony\Component\Form\Form')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $form->expects($this->exactly(1))->method('add')
+            ->with(
+                $this->equalTo('name'),
+                $this->equalTo('choice'),
+                $this->equalTo(
+                    array(
+                        'choices' => array(
+                            '1' => 'yes',
+                            '0' => 'no',
+                        ),
+                        'multiple' => false,
+                        'empty_value' => ''
+                    )
+                )
+            );
+
+        $options =  array();
 
         $method->invoke(
             $formFielExtension,
