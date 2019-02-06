e39b9a19904c3c04bfcf45430b19030deed03a18
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2019-02-06 12:30:55 +0100

    Do not use ManagerRegistry from Doctrine bridge

--- lib/Driver/Doctrine/ORM/Extension/Core/EventSubscriber/ResultIndexer.php
+++ lib/Driver/Doctrine/ORM/Extension/Core/EventSubscriber/ResultIndexer.php
@@ -7,51 +7,39 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace FSi\Component\DataSource\Driver\Doctrine\ORM\Extension\Core\EventSubscriber;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use FSi\Component\DataSource\Driver\Doctrine\ORM\DoctrineResult;
 use FSi\Component\DataSource\Event\DriverEvent\ResultEventArgs;
 use FSi\Component\DataSource\Event\DriverEvents;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-/**
- * Class contains method called at BindParameters events.
- */
 class ResultIndexer implements EventSubscriberInterface
 {
     /**
-     * @var \Symfony\Bridge\Doctrine\ManagerRegistry
+     * @var ManagerRegistry
      */
-    protected $registry;
+    private $registry;
 
-    /**
-     * @param \Symfony\Bridge\Doctrine\ManagerRegistry $registry
-     */
     public function __construct(ManagerRegistry $registry)
     {
         $this->registry = $registry;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public static function getSubscribedEvents()
     {
         return [DriverEvents::POST_GET_RESULT => ['postGetResult', 1024]];
     }
 
-    /**
-     * @param \FSi\Component\DataSource\Event\DriverEvent\ResultEventArgs $event
-     */
-    public function postGetResult(ResultEventArgs $event)
+    public function postGetResult(ResultEventArgs $event): void
     {
         $result = $event->getResult();
-
-        if ($result instanceof Paginator) {
-            $result = new DoctrineResult($this->registry, $result);
-            $event->setResult($result);
+        if (true === $result instanceof Paginator) {
+            $event->setResult(new DoctrineResult($this->registry, $result));
         }
     }
 }
